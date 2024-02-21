5e0f94d43d8442a3220a2725b3964a385eeb72e7
Mateusz Bukowski <mateuszbukowski93@gmail.com>
2024-02-21 10:27:25 +0100

    wishlist

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics, logEvent } from "firebase/analytics";
+import { getFirestore, setDoc, doc } from "firebase/firestore";
+import "firebase/firestore";
+import { v4 as uuidv4 } from "uuid";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC6OqL7AphHMU0i_Clvx8QsAW5WRIzHpzs",
   authDomain: "native-unnamed-app.firebaseapp.com",
@@ -20,3 +24,30 @@ export const firebaseLog = (eventLog) => {
   console.log({ eventLog });
   logEvent(analytics, { source: "landing_page", eventLog });
 };
+
+// Inicjalizacja Firestore
+export const firestore = getFirestore(app);
+/**
+ * Funkcja zapisująca obiekt w określonej kolekcji Firestore.
+ * @param {String} collectionName - Nazwa kolekcji, w której ma zostać zapisany obiekt.
+ * @param {Object} objectToSave - Obiekt, który ma zostać zapisany w kolekcji.
+ */
+export const saveObjectToCollection = async (collectionName, objectToSave) => {
+  const productRef = doc(firestore, collectionName, uuidv4());
+  await setDoc(productRef, objectToSave);
+};
+
+// export async function saveObjectToCollection(collectionName, objectToSave) {
+//   try {
+//     const docRef = doc(firestore, collectionName);
+//     await setDoc(docRef, objectToSave);
+//     console.log("Dokument zapisany z ID: ", docRef.id);
+//   } catch (error) {
+//     console.error("Błąd przy zapisie dokumentu: ", error);
+//   }
+// }
+
+// export const addNewTask = async (task: TaskType) => {
+//   const productRef = doc(firestore, 'tasks', task.id)
+//   await setDoc(productRef, task)
+// }
