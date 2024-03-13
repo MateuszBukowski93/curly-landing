64fae68eeb494543ea2dc7ce315a292c26668234
Mateusz Bukowski <mateuszbukowski93@gmail.com>
2024-03-13 21:58:06 +0100

    build fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,17 +7,12 @@ import { defineConfig, squooshImageService } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import config from "./src/config/config.json";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
-  output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
   image: {
     service: squooshImageService(),
   },
